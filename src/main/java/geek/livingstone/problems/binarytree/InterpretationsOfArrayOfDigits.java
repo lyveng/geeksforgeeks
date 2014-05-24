1f473d0fcb64ac18e0d814e73eeeb88c8249c12c
Emmanuel Livingstone <livingstone.s.e@gmail.com>
2014-05-24 15:47:48 +0530

    #10 Formatted the code.

--- src/main/java/geek/livingstone/problems/binarytree/InterpretationsOfArrayOfDigits.java
+++ src/main/java/geek/livingstone/problems/binarytree/InterpretationsOfArrayOfDigits.java
@@ -3,21 +3,20 @@ package geek.livingstone.problems.binarytree;
 import java.util.Arrays;
 
 public class InterpretationsOfArrayOfDigits {
-//For simplicity I am taking it as string array. Char Array will save space
-  private static final String[] alphabet = {"", "a", "b", "c", "d", "e",
-      "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
-      "s", "t", "u", "v", "w", "x", "v", "z"};
+  // For simplicity I am taking it as string array. Char Array will save space
+  private static final String[] alphabet = {"", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+      "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "v", "z"};
 
   public static Node createTree(int data, String curString, int[] arr) {
     if (data > 26)
       return null;
     Node node = new Node(curString + alphabet[data]);
-    if (arr.length > 0)
-    {
+    if (arr.length > 0) {
       node.left = createTree(arr[0], node.interpretation, Arrays.copyOfRange(arr, 1, arr.length));
-      if (arr.length > 1)
-      {
-        node.right = createTree(arr[0]*10 + arr[1], node.interpretation, Arrays.copyOfRange(arr, 2, arr.length));
+      if (arr.length > 1) {
+        node.right =
+            createTree(arr[0] * 10 + arr[1], node.interpretation,
+                Arrays.copyOfRange(arr, 2, arr.length));
       }
     }
     if (node.isLeafNode())
@@ -25,15 +24,15 @@ public class InterpretationsOfArrayOfDigits {
     return node;
   }
 
-  public static void printAllInterpretations(int[] arr)
-  {
-    System.out.println(String.format("Printing interpretations for array %s", Arrays.toString(arr)));
+  public static void printAllInterpretations(int[] arr) {
+    System.out
+        .println(String.format("Printing interpretations for array %s", Arrays.toString(arr)));
     createTree(0, "", arr);
     System.out.println();
   }
 
   public static void main(String[] args) {
- // aacd(1,1,3,4) amd(1,13,4) kcd(11,3,4)
+    // aacd(1,1,3,4) amd(1,13,4) kcd(11,3,4)
     // Note : 1,1,34 is not valid as we don't have values corresponding
     // to 34 in alphabet
     int[] arr = {1, 1, 3, 4};
@@ -47,11 +46,11 @@ public class InterpretationsOfArrayOfDigits {
     int[] arr3 = {2, 6};
     printAllInterpretations(arr3);
 
-    // ab(1,2), l(12)  
+    // ab(1,2), l(12)
     int[] arr4 = {1, 2};
     printAllInterpretations(arr4);
 
-    // a(1,0} j(10)  
+    // a(1,0} j(10)
     int[] arr5 = {1, 0};
     printAllInterpretations(arr5);
 
@@ -68,9 +67,11 @@ public class InterpretationsOfArrayOfDigits {
     String interpretation;
     Node left;
     Node right;
+
     Node(String interpretation) {
       this.interpretation = interpretation;
     }
+
     boolean isLeafNode() {
       return left == null && right == null;
     }
