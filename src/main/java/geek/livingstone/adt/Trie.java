3a845494fc491172c3bfcfdf25d179a706b05852
Emmanuel Livingstone <emmanuel.livingstone@deepvalue.net>
2014-06-04 01:46:20 -0400

    fixes #16 Added delete feature to trie implementation

--- src/main/java/geek/livingstone/adt/Trie.java
+++ src/main/java/geek/livingstone/adt/Trie.java
@@ -40,6 +40,50 @@ public class Trie {
     }
   }
 
+  /**
+   * Checks if there are any children for this trie.
+   * 
+   * @return
+   */
+  private boolean containsChildren() {
+    for (int i=0; i<ALPHABET_SIZE; i++)
+      if (children[i] != null)
+        return true;
+    return false;
+  }
+
+  private boolean deleteUtil(String key, int index) throws Exception {
+    if (index == key.length())
+    {
+      if (this.isLeaf) {
+        this.isLeaf = false;
+        // Case 2: The key is not a prefix of any other key. In that case. Returns true if no children.
+        // Case 3: Key is prefix of another longer key in trie. Returns false if atleast one child.
+        return !containsChildren();
+      }
+      // Case 1: Key is not present in trie
+      return false;
+    }
+    int childIndex = getIndexFromCharacter(key.charAt(index));
+    // Case 1: Key is not present in trie
+    if (children[childIndex] == null)
+      return false;
+    if (children[childIndex].deleteUtil(key, index+1))
+    {
+      children[childIndex] = null;
+      // Case 2: The key is unique key. return true to delete the key.
+      // Case 4: Key present in trie having atleast one another key as prefix or another key is part of this path. then return false
+      return !(containsChildren() || isLeaf);
+    }
+    return false;
+  }
+
+  public void delete(String key) throws Exception {
+    System.out.println();
+    System.out.println("Deleting key[" + key + "] from trie.");
+    deleteUtil(key, 0);
+  }
+
   public static void main(String[] args) throws Exception {
     String[] keys = {"the", "a", "there", "answer", "any", "by", "bye"};
     Trie root = new Trie();
@@ -50,6 +94,15 @@ public class Trie {
     System.out.println("these : " + root.search("these"));
     System.out.println("their : " + root.search("their"));
     System.out.println("thaw : " + root.search("thaw"));
+
+    root.delete(keys[0]);
+    System.out.println("the : " + root.search(keys[0]));
+    System.out.println("there : " + root.search(keys[2]));
+
+    root.delete(keys[2]);
+    System.out.println("the : " + root.search(keys[0]));
+    System.out.println("there : " + root.search(keys[2]));
+    System.out.println("answer : " + root.search(keys[3]));
   }
 
 }
