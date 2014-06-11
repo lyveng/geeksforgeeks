de672a4ab3b16d1e1a28b698f8f15cced7221cfd
Emmanuel Livingstone <emmanuel.livingstone@deepvalue.net>
2014-06-11 05:00:57 -0400

    Simplified the code for reversing alternate groups of linked list to one that uses reversing groups of linked list.

--- src/main/java/geek/livingstone/problems/linkedlist/ReverseLinkedListAlternateGroups.java
+++ src/main/java/geek/livingstone/problems/linkedlist/ReverseLinkedListAlternateGroups.java
@@ -11,35 +11,22 @@ import geek.livingstone.adt.LinkedListNode;
  */
 public class ReverseLinkedListAlternateGroups {
   public static <T extends Comparable<T>> void reverse(LinkedList<T> list, int k) {
-    list.setHead(reverseUtil(list.getHead(), k));
+    list.setHead(reverseUtil(list.getHead(), k, true));
   }
 
-  private static <T extends Comparable<T>> LinkedListNode<T> reverseUtil(LinkedListNode<T> node, int k) {
+  private static <T extends Comparable<T>> LinkedListNode<T> reverseUtil(LinkedListNode<T> node, int k, boolean reverse) {
     if (node == null) return null;
-    LinkedListNode<T> first = node, last = first, midFirst=null, midLast = first;
-    for (int i = 1; i < 2*k && last.getNext() != null; i++) {
-      if (i%k==0)
-      {
-        midLast = last;
-        midFirst = last.getNext();
-        last = last.getNext();
-        midLast.setNext(null);
-      }
-      else
-        last = last.getNext();
-      if (i < k)
-        midLast = last;
-    }
-    if (midLast == last)
-    {
-      last.setNext(null);
-      return reverseList(first);
-    }
+    LinkedListNode<T> first = node, last = first;
+    for (int i = 1; i < k && last.getNext() != null; i++)
+      last = last.getNext();
     LinkedListNode<T> next = last.getNext();
-    midLast.setNext(null);
+    if (!reverse) {
+      last.setNext(reverseUtil(next, k, true));
+      return first;
+    }
+    last.setNext(null);
     LinkedListNode<T> newFirst = reverseList(first);
-    last.setNext(reverseUtil(next, k));
-    first.setNext(midFirst);
+    first.setNext(reverseUtil(next, k, false));
     return newFirst;
   }
 
@@ -57,12 +44,11 @@ public class ReverseLinkedListAlternateGroups {
 
   public static void main(String[] args) {
     LinkedList<Integer> a = new LinkedList<>();
-    a.push(11);
-    a.push(10);
-    a.push(9);
-    a.push(8);
-    a.push(7);
-    a.push(6);
+    a.push(5);
+    a.push(4);
+    a.push(3);
+    a.push(2);
+    a.push(1);
     a.push(5);
     a.push(4);
     a.push(3);
