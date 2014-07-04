74a7fdf7b7dd84a61954b2b641a7da6ac76a3c5d
Emmanuel Livingstone <livingstone.s.e@gmail.com>
2014-07-04 15:53:13 +0530

    Added code for ternary search trees.

---
+++ src/main/java/geek/livingstone/adt/TernarySearchTree.java
@@ -0,0 +1,121 @@
+package geek.livingstone.adt;
+
+/**
+ * Full problem at http://www.geeksforgeeks.org/ternary-search-tree/
+ * 
+ * @author emmanuel
+ * 
+ */
+public class TernarySearchTree {
+  Node root;
+
+  public void insert(String value) {
+    if (root == null)
+      root = new Node(value, 0);
+    else
+      root.insert(value, 0);
+  }
+
+  public boolean search(String value) {
+    if (root == null)
+      return value == null || value.isEmpty();
+    return root.find(value, 0);
+  }
+
+  public void traverse() {
+    if (root != null)
+      root.traverse(new StringBuilder());
+  }
+
+  class Node {
+    char val;
+    Node left;
+    Node right;
+    Node down;
+    boolean isLeaf;
+
+    Node(String value, int index) {
+      assert index < value.length();
+      val = value.charAt(index);
+      if (index == value.length() - 1)
+        isLeaf = true;
+      else
+        down = new Node(value, index + 1);
+    }
+
+    void insert(String value, int index) {
+      assert index < value.length();
+      if (index == value.length() - 1) {
+        isLeaf = true;
+        return;
+      }
+      if (value.charAt(index) < val) {
+        if (left == null)
+          left = new Node(value, index);
+        else
+          left.insert(value, index);
+      } else if (value.charAt(index) == val)
+        if (down == null)
+          down = new Node(value, index+1);
+        else
+          down.insert(value, index + 1);
+      else {
+        if (right == null)
+          right = new Node(value, index);
+        else
+          right.insert(value, index);
+      }
+    }
+
+    boolean find(String value, int index) {
+      assert index < value.length();
+      if (index == value.length() - 1)
+        return isLeaf;
+      if (value.charAt(index) == val)
+        if (down == null)
+          return false;
+        else
+          return down.find(value, index + 1);
+      else if (value.charAt(index) < val)
+        if (left == null)
+          return false;
+        else
+          return left.find(value, index);
+      else if (right == null)
+        return false;
+      else
+        return right.find(value, index);
+    }
+
+    void traverse(StringBuilder sb) {
+      sb.append(val);
+      if (isLeaf)
+        System.out.println(sb.toString());
+      if (down != null)
+        down.traverse(sb);
+      sb.deleteCharAt(sb.length()-1);
+      if (left != null)
+        left.traverse(sb);
+      if (right != null)
+        right.traverse(sb);
+    }
+  }
+
+  public static void main(String[] args) {
+    TernarySearchTree tst = new TernarySearchTree();
+    tst.insert("cat");
+    tst.insert("cats");
+    tst.insert("up");
+    tst.insert("bug");
+    
+    System.out.println("Following is the traversal of ternary search tree");
+    tst.traverse();
+
+    System.out.println("\nFollowing are search results for cats, bu and cat respectively");
+    System.out.println(tst.search("cats"));
+    System.out.println(tst.search("bu"));
+    System.out.println(tst.search("cat"));
+    System.out.println(tst.search("sads"));
+  }
+
+}
