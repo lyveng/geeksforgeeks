fdab1dcc6399b4e0f77613d763dd27596b2ff75f
Emmanuel Livingstone <emmanuel.livingstone@deepvalue.net>
2014-07-17 02:36:06 -0400

    Palindrome Partitioning Revision 2

--- src/main/java/geek/livingstone/problems/dp/PalindromePartitioning.java
+++ src/main/java/geek/livingstone/problems/dp/PalindromePartitioning.java
@@ -1,5 +1,6 @@
 package geek.livingstone.problems.dp;
 
+
 /**
  * Full problem can be found at
  * http://www.geeksforgeeks.org/dynamic-programming-set-17-palindrome-partitioning/
@@ -8,61 +9,46 @@ package geek.livingstone.problems.dp;
  * 
  */
 public class PalindromePartitioning {
-  private Boolean[][] isPalindrome;
-
-  public PalindromePartitioning(String s) {
-    isPalindrome = new Boolean[s.length()][s.length()];
-    if (isPalindrome(s)) {
-      System.out.println(0);
-      return;
+  public static void partition(String s) {
+    int[][] c = new int[s.length()][s.length()];
+    for (int i = 0; i < c.length; i++) {
+      for (int j = 0; j < c[i].length; j++) {
+        c[i][j] = i == j ? 0 : Integer.MAX_VALUE;
+      }
     }
-    int[][] min = new int[s.length()][s.length()];
-    for (int i = 0; i < s.length(); i++) {
-      min[i][i] = 0;
-      isPalindrome[i][i] = true;
-    }
-    for (int L = 2; L <= s.length(); L++) {
-      for (int i = 0; i <= (s.length() - L); i++) {
-        int j = i + L - 1;
-        if (s.charAt(i) == s.charAt(j) && L == 2)
-          isPalindrome[i][j] = true;
-        else if (s.charAt(i) == s.charAt(j))
-          isPalindrome[i][j] = isPalindrome[i + 1][j - 1];
-        else
-          isPalindrome[i][j] = false;
-        if (isPalindrome[i][j]) {
-          min[i][j] = 0;
+    for (int l = 2; l <= s.length(); l++) {
+      for (int start = 0; start <= s.length() - l; start++) {
+        int end = start + l - 1;
+        if (isPalindrome(s, start, end)) {
+          c[start][end] = 0;
           continue;
         }
-        min[i][j] = Integer.MAX_VALUE;
-        for (int k = i; k < j; k++) {
-          int minK = 1 + min[i][k] + min[k + 1][j];
-          if (minK < min[i][j]) min[i][j] = minK;
+        for (int cut = start; cut < end; cut++) {
+          c[start][end] = Math.min(c[start][end], 1 + c[start][cut] + c[cut + 1][end]);
         }
       }
     }
-    System.out.println(min[0][s.length() - 1]);
+    System.out.println(c[0][s.length() - 1]);
   }
 
-  private boolean isPalindrome(String s) {
-    if (s.length() <= 1) return true;
-    int i = (s.length() - 2) / 2;
-    int j = (s.length() % 2 == 0) ? (i + 1) : (i + 2);
-    for (; i >= 0 && j < s.length(); i--, j++)
-      if (s.charAt(i) != s.charAt(j)) {
-        return false;
-      }
-    return true;
+  private static boolean isPalindrome(String s, int start, int end) {
+    while (start < end) {
+      if (s.charAt(start) != s.charAt(end))
+        break;
+      start++;
+      end--;
+    }
+    return start >= end;
   }
 
   public static void main(String[] args) {
-    new PalindromePartitioning("a");
-    new PalindromePartitioning("ab");
-    new PalindromePartitioning("aba");
-    new PalindromePartitioning("abc");
-    new PalindromePartitioning("abba");
-    new PalindromePartitioning("abbc");
-    new PalindromePartitioning("ababbbabbababa");
+    partition("a");
+    partition("ab");
+    partition("aba");
+    partition("abc");
+    partition("abba");
+    partition("abbc");
+    partition("ababbbabbababa");
   }
 
 }
