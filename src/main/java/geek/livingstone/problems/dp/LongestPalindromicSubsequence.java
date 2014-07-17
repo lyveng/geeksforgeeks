3006a956e91583d852721f6eb4fc7f0483852479
Emmanuel Livingstone <emmanuel.livingstone@deepvalue.net>
2014-07-17 00:45:29 -0400

    LPS revision 2.

--- src/main/java/geek/livingstone/problems/dp/LongestPalindromicSubsequence.java
+++ src/main/java/geek/livingstone/problems/dp/LongestPalindromicSubsequence.java
@@ -2,29 +2,29 @@ package geek.livingstone.problems.dp;
 
 import java.util.Arrays;
 
+
 public class LongestPalindromicSubsequence {
-  public LongestPalindromicSubsequence(String s) {
-    int[][] L = new int[s.length()][s.length()];
-    for (int i=0;i<s.length();i++)
-      L[i][i] = 1;
-    // sl is the length of the subsequence
-    for (int sl=2;sl<=s.length();sl++) {
-      for (int i=0;i<=s.length()-sl;i++) {
-        int j=i+sl-1;
-        if (s.charAt(i)==s.charAt(j) && sl==2)
-          L[i][j] = 2;
-        else if (s.charAt(i)==s.charAt(j))
-          L[i][j] = 2 + L[i+1][j-1];
+  public static void lps(String s) {
+    int[][] lps = new int[s.length()][s.length()];
+    for (int i = 0; i < lps.length; i++) {
+      lps[i][i] = 1;
+    }
+    for (int length = 2; length <= s.length(); length++) {
+      for (int start = 0; start <= s.length() - length; start++) {
+        int end = start + length - 1;
+        if (s.charAt(start) == s.charAt(end))
+          lps[start][end] = 2 + lps[start+1][end-1];
         else
-          L[i][j] = Math.max(L[i+1][j], L[i][j-1]);
+          lps[start][end] = Math.max(lps[start+1][end], lps[start][end-1]);
       }
     }
-    for (int i=0;i<s.length();i++)
-      System.out.println(Arrays.toString(L[i]));
-    System.out.println("Length of LPS is " + L[0][s.length()-1]);
+    for (int i = 0; i < lps.length; i++) {
+      System.out.println(Arrays.toString(lps[i]));
+    }
+    System.out.println(lps[0][s.length()-1]);
   }
 
   public static void main(String[] args) {
-    new LongestPalindromicSubsequence("BBABCBCAB");
+    lps("BBABCBCAB");
   }
 }
