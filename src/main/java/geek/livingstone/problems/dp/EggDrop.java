f578bb778ad90b79bd9213648d0a170df87a16c3
Emmanuel Livingstone <emmanuel.livingstone@deepvalue.net>
2014-07-17 00:07:04 -0400

    Added simple code for egg dropping puzzle.

--- src/main/java/geek/livingstone/problems/dp/EggDrop.java
+++ src/main/java/geek/livingstone/problems/dp/EggDrop.java
@@ -3,45 +3,37 @@ package geek.livingstone.problems.dp;
 import java.util.Arrays;
 
 public class EggDrop {
-
-  public EggDrop(int n, int k) {
-    int[][] E = new int[n+1][k+1];
-    for (int i=0;i<=n;i++) {
-      // 1 floor means 1 trial irrespective of number of eggs..
-      E[i][1] = 1;
-      // 0 floors means 0 trials.
-      E[i][0] = 0;
+  /**
+   * n eggs k floors
+   * @param n
+   * @param k
+   */
+  public static void eggDrop(int n, int k) {
+    int[][] m = new int[n+1][k+1];
+    // 0 floors - 0 trials - already set by default
+    // 1 floor - 1 trial
+    // 0 eggs - Pos Inf
+    for (int i = 1; i < k; i++) {
+      m[0][i] = Integer.MAX_VALUE;
     }
-    for (int j=0;j<=k;j++) {
-      // 0 eggs means no trials
-      E[0][j] = 0;
-      // 1 egg means i trials for i floors.
-      E[1][j] = j;
-    }
-    int xVal;
-    for (int i=2;i<=n;i++) {
-      for (int j=2;j<=k;j++) {
-        E[i][j] = Integer.MAX_VALUE;
-        int minFloor = 0;
-        for (int x=1;x<=j;x++) {
-          xVal = 1 + Math.max(E[i-1][x-1], E[i][j-x]);
-          if (xVal < E[i][j]) {
-            minFloor = x;
-            E[i][j] = xVal;
-          }
+    for (int eggs = 1; eggs <= n; eggs++) {
+      for (int floors = 1; floors <= k; floors++) {
+        m[eggs][floors] = Integer.MAX_VALUE;
+        for (int x = 1; x <= floors; x++) {
+          m[eggs][floors] = Math.min(m[eggs][floors], Math.max(m[eggs-1][x-1], m[eggs][floors-x]));
         }
-        System.out.println(String.format("For eggs=%s and floors=%s floor to begin with is %s", i, j, minFloor));
+        if (m[eggs][floors] != Integer.MAX_VALUE)
+          m[eggs][floors] += 1;
       }
     }
-    for (int i=0;i<=n;i++) {
-      System.out.println(Arrays.toString(E[i]));
+    for (int i = 0; i < m.length; i++) {
+      System.out.println(Arrays.toString(m[i]));
     }
-    
-    System.out.println(E[n][k]);
+    System.out.println(m[n][k]);
   }
 
   public static void main(String[] args) {
-    new EggDrop(2, 10);
+    eggDrop(2, 10);
   }
 
 }
